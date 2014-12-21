8707ff9eb353ce89edff8976151dfd72692e42a0
Thomas Wiringa <thomas.wiringa@gmail.com>
2014-12-21 23:49:43 +0100

    Changed identation

--- app/database/migrations/2014_12_21_170220_create_users_table.php
+++ app/database/migrations/2014_12_21_170220_create_users_table.php
@@ -14,17 +14,17 @@ class CreateUsersTable extends Migration {
 	{
 		Schema::create('users', function(Blueprint $table)
 		{
-			$table->increments('id'); 				// Autoincrement	: ID
-			$table->integer('login')->unique(); 	// INTEGER			: Username
-			$table->string('company', 255);			// VARCHAR(255)		: Company Name
-			$table->string('street', 100);			// VARCHAR(100)		: Street + Nr
-			$table->string('postcode', 7); 			// VARCHAR(7)		: Postcode: XXXX YY
-			$table->string('city', 50);				// VARCHAR(50)		: City
-			$table->string('email', 50); 			// VARCHAR(50)		: Email address
-			$table->boolean('active');				// BOOLEAN			: Active (if this is true, the user is able to login)
-			$table->string('password');				// VARCHAR()		: Password
-			$table->mediumText('favorites');		// MEDIUMTEXT		: A serialized array containing the favorite products from the user
-			$table->mediumText('cart');				// MEDIUMTEXT		: The cart is saved here between sessions
+			$table->increments('id'); 		// Autoincrement : ID
+			$table->integer('login')->unique(); 	// INTEGER	 : Username
+			$table->string('company', 255);		// VARCHAR(255)	 : Company Name
+			$table->string('street', 100);		// VARCHAR(100)	 : Street + Nr
+			$table->string('postcode', 7); 		// VARCHAR(7)	 : Postcode: XXXX YY
+			$table->string('city', 50);		// VARCHAR(50)	 : City
+			$table->string('email', 50); 		// VARCHAR(50)	 : Email address
+			$table->boolean('active');		// BOOLEAN	 : Active (if this is true, the user is able to login)
+			$table->string('password');		// VARCHAR()	 : Password
+			$table->mediumText('favorites');	// MEDIUMTEXT	 : A serialized array containing the favorite products from the user
+			$table->mediumText('cart');		// MEDIUMTEXT	 : The cart is saved here between sessions
 			$table->timestamps();
 		});
 	}
