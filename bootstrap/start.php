ad52a6fb9a77f596eb2953a8a15034acc9882858
Thomas Wiringa <thomas.wiringa@gmail.com>
2014-12-22 00:01:15 +0100

    Added more development stages

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,9 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead', 'Macbook-Pro-Luna', 'Hackintosh-Luna'),
+	'local'         => array('homestead', '*.local'),
+        'staging'       => array('luna-server'),
+        'production'    => array('key2publish.nl'),
 
 ));
 
