a7efcd216468eb22d2b5b73687a7f7c3aab2c60e
Thomas Wiringa <thomas.wiringa@gmail.com>
2014-12-21 20:55:41 +0100

    Added more hostnames to the bootstrap file

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('Macbook-Pro-Luna'),
+	'local' => array('homestead', 'Macbook-Pro-Luna', 'Hackintosh-Luna'),
 
 ));
 
