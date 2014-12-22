47ec4dbfbe4325edba089c599e2d921250cf4186
Thomas Wiringa <thomas.wiringa@gmail.com>
2014-12-22 17:29:04 +0100

    Added routes

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,17 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+// GET Requests will be handled here
+Route::get('/', 'HomeController@home');                                 // Homepage
+Route::get('/about', 'HomeController@about');                           // About us
+Route::get('/contact', 'HomeController@contact');                       // Contact
+Route::get('/downloads', 'HomeController@downloads');                   // Downloads
+
+Route::get('/login', 'WebshopController@loginPage');                    // Login page
+Route::get('/forgotPassword', 'WebshopController@resetPassword');       // Forgot password page
+Route::get('/webshop', 'WebshopController@search');                     // Main webshop page
+Route::get('/product/{product_id}', 'WebshopController@showProduct');   // Product page
+
+// POST Requests will be handeled here
+Route::post('/login', 'LoginController@login');                         // Login handler
+Route::post('/forgotPassword', 'LoginController@resetPassword');        // Password reset handler
