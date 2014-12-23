c86ad1b16560336c51947e70c9f8f62fa3449cc5
Thomas Wiringa <thomas.wiringa@gmail.com>
2014-12-24 00:32:20 +0100

    Added Contact page Added About Us page

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,23 +2,39 @@
 
 class HomeController extends BaseController {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
+        /*
+        |--------------------------------------------------------------------------
+        | Default Home Controller
+        |--------------------------------------------------------------------------
+        |
+        | You may wish to use controllers instead of, or in addition to, Closure
+        | based routes. That's great! Here is an example controller method to
+        | get you started. To route to this controller, just add the route:
+        |
+        |	Route::get('/', 'HomeController@showWelcome');
+        |
+        */
 
-	public function home()
-	{
-		return View::make('home.index');
-	}
+        public function home()
+        {
+                return View::make('home.index');
+        }
 
+        public function about()
+        {
+                $manufacturers = json_decode(file_get_contents(public_path() . "/json/manufacturers.json"));
+
+                return View::make('home.about', array('manufacturers' => $manufacturers));
+        }
+
+        public function contact()
+        {
+                return View::make('home.contact');
+        }
+
+        public function downloads()
+        {
+                return View::make('home.downloads');
+        }
 
 }
---
+++ app/views/home/contact.blade.php
@@ -0,0 +1,41 @@
+@extends('master')
+
+@section('title')
+        <h3>Contact</h3>
+@stop
+
+@section('content')
+        <table class="table table-striped table-hover">
+                <tbody>
+                        <tr>
+                                <td><b>Telefoon:</b></td>
+                                <td>050-5445566</td>
+                                <td>08:00 - 17:00</td>
+                        </tr>
+                        <tr>
+                                <td><b>Balie:</b></td>
+                                <td>Maandag t/m Vrijdag</td>
+                                <td>07:30 - 17:00</td>
+                        </tr>
+                        <tr>
+                                <td><b>Fax:</b></td>
+                                <td>050-5445565</td>
+                                <td></td>
+                        </tr>
+                        <tr>
+                                <td><b>E-Mail:</b></td>
+                                <td>verkoop@wiringa.nl</td>
+                                <td></td>
+                        </tr>
+                        <tr>
+                                <td><b>Adres:</b></td>
+                                <td>Bovenstreek 1</td>
+                                <td>Groningen</td>
+                        </tr>
+                </tbody>
+        </table>
+
+        <div class="hidden-xs">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2387.994500319521!2d6.590588013491823!3d53.23587489278383!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c9d29bd46ee021%3A0x9589c80e5b3dd396!2sBovenstreek+1%2C+9731+DH+Groningen!5e0!3m2!1snl!2snl!4v1389359520881" style="width: 100%;" seamless="" frameborder="0" height="600"></iframe>
+        </div>
+@stop
