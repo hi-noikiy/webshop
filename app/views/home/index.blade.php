07a646ff79c7a42d70620fda92bb0667cb637217
Thomas Wiringa <thomas.wiringa@gmail.com>
2014-12-23 01:21:01 +0100

    Added homepage view Added HomController controller Added less folder/file Added master template Added img folder Added .gitignore to img folder
    Removed .gitignore from less folder
    
    Updated CSS

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -1,13 +1,84 @@
 @extends('master')
 
-@section('header')
-    Header
+@section('title')
+        <h1>Welkom op de website van Wiringa Technische Groothandel</h1>
 @stop
 
 @section('content')
-    Content
-@stop
+        <div class="row">
+                <div class="col-md-5">
+                        <div id="slideshow" class="carousel slide">
+                                <ol class="carousel-indicators">
+                                        <li data-target="#slideshow" data-slide-to="0" class=""></li>
+                                        <li data-target="#slideshow" data-slide-to="1" class=""></li>
+                                        <li data-target="#slideshow" data-slide-to="2" class=""></li>
+                                        <li data-target="#slideshow" data-slide-to="3" class="active"></li>
+                                        <li data-target="#slideshow" data-slide-to="4" class=""></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                        <div class="item">
+                                                <img src="/img/carousel/profactie.jpg" alt="profactie.jpg">
+                                                <div class="carousel-caption">
+                                                        <h3>Viega Profipress actiepakket</h3>
+                                                        <p>Viega Profipress actiepakket voor een scherpe prijs!!!</p>
+                                                </div>
+                                        </div>
+                                        <div class="item">
+                                                <img src="/img/carousel/prestactie.jpg" alt="prestactie.jpg">
+                                                <div class="carousel-caption">
+                                                        <h3>Viega Prestabo actiepakket</h3>
+                                                        <p>Viega Prestabo actiepakket voor een scherpe prijs!!!</p>
+                                                </div>
+                                        </div>
+                                        <div class="item">
+                                                <img src="/img/carousel/182150050.jpg" alt="182150050.jpg">
+                                                <div class="carousel-caption">
+                                                        <h3>Blucher afvoerputten</h3>
+                                                        <p>Blucher vloerputten programma</p>
+                                                </div>
+                                        </div>
+                                        <div class="item active">
+                                                <img src="/img/carousel/ra280326.jpg" alt="ra280326.jpg">
+                                                <div class="carousel-caption">
+                                                        <h3>Raminex terugstroombeveiligingen</h3>
+                                                        <p>Kogelkraan met keerklep in easy service (ES) uitvoering.</p>
+                                                </div>
+                                        </div>
+                                        <div class="item">
+                                                <img src="/img/carousel/bu400480100.jpg" alt="bu400480100.jpg">
+                                                <div class="carousel-caption">
+                                                        <h3>EcoFlo</h3>
+                                                        <p>Tapwater opwarmen via het rookgaskanaal. Nieuw van Burgerhout.</p>
+                                                </div>
+                                        </div>
 
-@section('footer')
-    Footer
+                                </div>
+                                <a class="carousel-control left" href="#slideshow" data-slide="prev"><span class="icon-prev"></span></a>
+                                <a class="carousel-control right" href="#slideshow" data-slide="next"><span class="icon-next"></span></a>
+                        </div>
+                </div>
+
+                <div class="col-md-7">
+                        @if(Auth::check())
+                                <p>Welkom op de vernieuwde website. De belangrijkste wijziging is onze nieuwe webshop.</p>
+                                <p>Mocht u een rechtstreekse link gebruiken naar de oude webwinkel dan zal deze nu niet meer werken.</p>
+                        @else
+                                <div class="row">
+                                        <div class="col-md-7">
+                                                <p>
+                                                        Welkom op de vernieuwde website. <br />
+                                                        De belangrijkste wijziging is onze nieuwe webshop.
+                                                </p>
+                                                <p>Mocht u een rechtstreekse link gebruiken naar de oude webwinkel dan zal deze nu niet meer werken.</p>
+                                        </div>
+                                        <div class="col-md-5">
+                                                <div class="well well-sm text-center">
+                                                        <h3>Klant worden?</h3>
+                                                        <a class="btn btn-block btn-success" href="/register">Ja, ik wil klant worden!</a>
+                                                </div>
+                                        </div>
+                                </div>
+                        @endif
+                </div>
+        </div>
 @stop
--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -2,25 +2,98 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Laravel PHP Framework</title>
+	<title>WTG</title>
 	<style>
-		@import url(//fonts.googleapis.com/css?family=Lato:700);
-        @import url(//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css);
+		@import url(//fonts.googleapis.com/css?family=Lato:300);
+                @import url(//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css);
+                @import url(//localhost:8000/css/app.min.css);
 	</style>
 </head>
 <body>
-    <div class="content">
-       <header>
-           @yield('header')
-       </header>
+        <nav class="navbar navbar-wtg navbar-static-top" role="navigation">
+                <div class="container">
+                        <div class="navbar-header">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+                                        <span class="sr-only">Toggle navigation</span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                </button>
+                                <a class="navbar-brand" href="/">
+                                        WTG
+                                </a>
+                        </div>
 
-        @yield('content')
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="navbar">
+                                <ul class="nav navbar-nav">
+                                        <li class="@if( Route::current()->getUri() === '/' ) active @endif"><a href="/">Home</a></li>
+                                        <li class="dropdown">
+                                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Info <span class="caret"></span></a>
+                                                <ul class="dropdown-menu" role="menu">
+                                                        <li><a href="/about">Over ons</a></li>
+                                                        <li><a href="/contact">Contact</a></li>
+                                                 </ul>
+                                        </li>
+                                        <li class="@if( Route::current()->getUri() === '/downloads' ) active @endif"><a href="/downloads">Downloads</a></li>
+                                        <li class="dropdown">
+                                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Webshop <span class="caret"></span></a>
+                                                <ul class="dropdown-menu" role="menu">
+                                                        <li><a href="/webshop">Bestellen</a></li>
+                                                        <li><a href="/special">Acties</a></li>
+                                                        <li><a href="/clearance">Opruiming</a></li>
+                                                </ul>
+                                        </li>
+                                </ul>
 
-        <footer>
-            @yield('footer')
+                                <ul class="nav navbar-nav navbar-right">
+                                        <li><a href="/login">Login</a></li>
+                                </ul>
 
-            <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-        </footer>
-    </div>
+                                <form action="/search" method="POST" class="navbar-form navbar-right hidden-xs" role="search">
+                                        <ul class="nav navbar-nav">
+                                                <li class="search-field">
+                                                        <input type="text" id="searchInput" class="form-control search-field" placeholder="Zoeken" value="Zoeken" name="search" maxlength="15" title="Geef een zoekopdracht van minimaal 3 en maximaal 15 tekens op." required>
+                                                        <span class="glyphicon glyphicon-search" style="color: #fff; cursor: pointer;"></span>
+                                                </li>
+                                        </ul>
+                                </form>
+                        </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+        </nav>
+
+        <header class="page-header">
+                <div class="container">
+                        <div class="col-md-4">
+                                <a href="/"><img src="/img/logo.png" alt="Logo"></a>
+                        </div>
+                        <div class="col-md-6">
+                                <h3>Sinds 1956 uw inkoop gemak van bodem tot dak. Uw partner voor non-ferro, leidingsystemen, dakbedekking, sanitair en appendages.</h3>
+                        </div>
+                </div>
+        </header>
+
+        <div class="container content">
+
+                <div class="row">
+                        <div class="col-md-12 bg-primary site-title">
+                                @yield('title')
+                        </div>
+                </div>
+
+                @yield('content')
+
+                <hr />
+
+                <footer>
+                        <div class="text-center">
+                                <div class="glyphicon glyphicon-copyright-mark"></div> Wiringa Technische Groothandel (2014 - 2015) - <a href="#">Thomas Wiringa</a> - <a href="http://wiringa.nl/">wiringa.nl</a>
+                                <p><small>Alle leveringen geschieden volgens onze algemene verkoopvoorwaarden gedeponeerd ter griffie van de arrondissementsrechtbank te Groningen onder nummer HK-255/93.</small></p>
+                        </div>
+
+                        <script src="//code.jquery.com/jquery-2.1.3.min.js"></script>
+                        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+                </footer>
+        </div>
 </body>
 </html>
