9c6b98b0c8c92c60f00eae4670b8f7acd435979c
Thomas Wiringa <thomas.wiringa@gmail.com>
2015-01-06 20:10:03 +0100

    Changed route name

--- app/routes.php
+++ app/routes.php
@@ -19,15 +19,22 @@ Route::get('/downloads', 'HomeController@downloads');                   // Downl
 
 Route::get('/login', 'WebshopController@loginPage');                    // Login page
 Route::get('/logout', 'WebshopController@logout');                      // Logout the current user
-Route::get('/forgotPassword', 'WebshopController@resetPassword');       // Forgot password page
+Route::get('/forgotpassword', 'WebshopController@forgotPassword');      // Forgot password page
 Route::get('/webshop', 'WebshopController@main');                       // Main webshop page
 Route::get('/product/{product_id}', 'WebshopController@showProduct');   // Product page
 Route::get('/search', 'WebshopController@search');                      // Page with the search results
 Route::get('/specials', 'WebshopController@specials');                  // Show only the specials
 Route::get('/clearance', 'WebshopController@clearance');                // Show only the clearance products
 
+Route::get('/account', 'AccountController@overview');                   // Account overview
+Route::get('/account/changepassword', 'AccountController@changePass');  // Change password page
+Route::get('/account/favorites', 'AccountController@favorites');        // Favorites
+Route::get('/account/history', 'AccountController@history');            // Order history
+Route::get('/account/addresslist', 'AccountController@addresslist');    // Addresslist
+Route::get('/account/discountfile', 'AccountController@discountfile');  // ICC/CSV Discount generation page
+
 Route::get('/cart', 'CartController@view');                             // Show the cart
 
 // POST Requests will be handled here
 Route::post('/login', 'WebshopController@login');                       // Login handler
-Route::post('/forgotPassword', 'WebshopController@resetPassword');      // Password reset handler
+Route::post('/forgotpassword', 'WebshopController@resetPassword');      // Password reset handler
