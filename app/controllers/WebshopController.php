227da3c54d9d479b650db3dad524470c8197d733
Thomas Wiringa <thomas.wiringa@gmail.com>
2015-01-06 17:36:28 +0100

    Added functions for the clearance and specials pages

--- app/controllers/WebshopController.php
+++ app/controllers/WebshopController.php
@@ -8,11 +8,11 @@ class WebshopController extends BaseController {
         |--------------------------------------------------------------------------
         |
         | This controller will process the requests for the pages:
-        |       - Login
+        |       - Login/Logout
         |       - Reset Password
         |       - Main pages
         |       - Product page
-        |
+        |       - Search
         */
 
         /**
@@ -87,19 +87,11 @@ class WebshopController extends BaseController {
                 $series =
                 $types = array();
 
-                // Get the brands from the search results
-                foreach ($allResults as $brand) {
-                        $brands[] = $brand->brand;
-                }
-
-                // Get the series from the search results
-                foreach ($allResults as $serie) {
-                        $series[] = $serie->series;
-                }
-
-                // Get the series from the search results
-                foreach ($allResults as $type) {
-                        $types[] = $type->type;
+                // Get the brands, series and types from the search results
+                foreach ($allResults as $product) {
+                        $brands[] = $product->brand;
+                        $series[] = $product->series;
+                        $types[] = $product->type;
                 }
 
                 // Return the search view with the fetched data
@@ -122,11 +114,22 @@ class WebshopController extends BaseController {
          */
         public function showProduct($product_Id)
         {
-                $product = Product::where('number', $product_Id)->firstOrFail();
+                $product  = Product::where('number', $product_Id)->firstOrFail();
+                $discount = (Auth::check() ? getProductDiscount(Auth::user()->login, $product->group, $product->number) : null);
 
-                return View::make('webshop.product', array('productData' => $product));
+                return View::make('webshop.product', array(
+                                'productData' => $product,
+                                'discount' => $discount
+                        )
+                );
         }
 
+        /**
+         * The user will be redirected to the previous page with
+         * a message indicating whether the login was successful or not
+         *
+         * @return mixed
+         */
         public function login()
         {
                 if (Input::has('username') && Input::has('password'))
@@ -134,14 +137,19 @@ class WebshopController extends BaseController {
                         if (Auth::attempt(array('login' => Input::get('username'), 'password' => Input::get('password'), 'active' => 1), true))
                         {
                                 Session::put('id', Auth::user()->login);
+                                return Redirect::back()->with('success', 'U bent nu ingelogd');
                         }
-                } else
-                {
-                        // The input field(s) is/are empty, go back to the previous page with an error message
-                        return Redirect::back()->with('error', 'Gebruikersnaam en/of wachtwoord onjuist')->with('username', Input::get('username'));
                 }
+
+                // The input field(s) is/are empty, go back to the previous page with an error message
+                return Redirect::back()->with('error', 'Gebruikersnaam en/of wachtwoord onjuist')->with('username', Input::get('username'));
         }
 
+        /**
+         * The user will be redirected to the main page
+         *
+         * @return mixed
+         */
         public function logout()
         {
                 if (Auth::check())
@@ -153,5 +161,101 @@ class WebshopController extends BaseController {
                 {
                         return Redirect::to('/')->with('error', 'Geen gebruiker ingelogd');
                 }
+        }
+
+        /**
+         * A search page only searching for the specials
+         *
+         * @return mixed
+         */
+        public function specials()
+        {
+                $startTime = microtime(true);
+                $inputBrand = Input::get('brand');
+                $inputSerie = Input::get('serie');
+                $inputType = Input::get('type');
+
+                $query = DB::table('products')->Where('action_type', 'Actie');
+
+                if (Input::has('brand')) $query = $query->where('brand', $inputBrand);
+                if (Input::has('serie')) $query = $query->where('series', $inputSerie);
+                if (Input::has('type'))  $query = $query->where('type', $inputType);
+
+                // Get all the results to filter the brands, series and types from it
+                $allResults = $query->orderBy('number', 'asc')->get();
+
+                // Get the paginated results
+                $results = $query->paginate(25);
+
+                // Initialize $brands, $series, $types as array
+                $brands =
+                $series =
+                $types = array();
+
+                // Get the brands, series and types from the search results
+                foreach ($allResults as $product) {
+                        $brands[] = $product->brand;
+                        $series[] = $product->series;
+                        $types[] = $product->type;
+                }
+
+                // Return the search view with the fetched data
+                return View::make('webshop.altSearch', array(
+                                'results'       => $results,
+                                'formAction'    => 'specials',
+                                'brands'        => array_unique($brands),
+                                'series'        => array_unique($series),
+                                'types'         => array_unique($types),
+                                'scriptTime'    => round(microtime(true) - $startTime, 4)
+                        )
+                );
+        }
+
+        /**
+         * A search page only searching for the clearance products
+         *
+         * @return mixed
+         */
+        public function clearance()
+        {
+                $startTime = microtime(true);
+                $inputBrand = Input::get('brand');
+                $inputSerie = Input::get('serie');
+                $inputType = Input::get('type');
+
+                $query = DB::table('products')->Where('action_type', 'Opruiming');
+
+                if (Input::has('brand')) $query = $query->where('brand', $inputBrand);
+                if (Input::has('serie')) $query = $query->where('series', $inputSerie);
+                if (Input::has('type'))  $query = $query->where('type', $inputType);
+
+                // Get all the results to filter the brands, series and types from it
+                $allResults = $query->orderBy('number', 'asc')->get();
+
+                // Get the paginated results
+                $results = $query->paginate(25);
+
+                // Initialize $brands, $series, $types as array
+                $brands =
+                $series =
+                $types = array();
+
+                // Get the brands, series and types from the search results
+                foreach ($allResults as $product) {
+                        $brands[] = $product->brand;
+                        $series[] = $product->series;
+                        $types[] = $product->type;
+                }
+
+                // Return the search view with the fetched data
+                return View::make('webshop.altSearch', array(
+                                'results'       => $results,
+                                'formAction'    => 'clearance',
+                                'brands'        => array_unique($brands),
+                                'series'        => array_unique($series),
+                                'types'         => array_unique($types),
+                                'scriptTime'    => round(microtime(true) - $startTime, 4)
+                        )
+                );
         }
 }
