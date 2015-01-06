40e52b3d402782f9d75590db0bdce25e6938911b
Thomas Wiringa <thomas.wiringa@gmail.com>
2015-01-06 17:35:32 +0100

    Added view for HTTP 500 errors

---
+++ app/views/errors/500.blade.php
@@ -0,0 +1,12 @@
+@extends('master')
+
+@section('title')
+        <h3>Error 500</h3>
+@stop
+
+@section('content')
+        <div class="alert alert-danger" role="alert">
+                Er is een foutopgetreden waardoor de pagina niet kon worden geladen.<Br />
+                Wij zijn er van op de hoogte er zullen het probleem zo spoedig mogelijk verhelpen.
+        </div>
+@stop
