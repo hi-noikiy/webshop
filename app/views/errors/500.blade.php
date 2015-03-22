44acb2cb1749eee0ee632a6625618f95875464cf
Thomas Wiringa <thomas.wiringa@gmail.com>
2015-03-22 16:57:17 +0100

    Fixed error message

--- app/views/errors/500.blade.php
+++ app/views/errors/500.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
         <div class="alert alert-danger" role="alert">
-                Er is een foutopgetreden waardoor de pagina niet kon worden geladen.<Br />
+                Er is een fout opgetreden waardoor de pagina niet kon worden geladen.<Br />
                 Wij zijn er van op de hoogte er zullen het probleem zo spoedig mogelijk verhelpen.
         </div>
 @stop
