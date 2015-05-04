158d9ef9058cf338c7d4cda1ed8d4cc7eafc0427
Thomas Wiringa <thomas.wiringa@gmail.com>
2015-05-04 13:32:53 +0200

    Fixed function

--- app/views/errors/404.blade.php
+++ app/views/errors/404.blade.php
@@ -6,6 +6,6 @@
 
 @section('content')
         <div class="alert alert-danger" role="alert">
-                De pagina, {{ URI::path() }}, is niet gevonden op de server.
+                De pagina, {{ Request::path() }}, is niet gevonden op de server.
         </div>
 @stop
