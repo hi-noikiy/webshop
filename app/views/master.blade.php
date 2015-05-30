051159dba5b3d9c3a0ad8e5c043e99d6c3eb8942
Thomas Wiringa <thomas.wiringa@gmail.com>
2015-05-30 13:03:23 +0200

    Changed header alignment

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -137,11 +137,11 @@
 
         <header class="page-header hidden-xs">
                 <div class="container">
-                        <div class="col-md-4">
+                        <div class="col-md-5 header-logo">
                                 <a href="/"><img src="/img/logo.png" alt="Logo"></a>
                         </div>
-                        <div class="col-md-8">
-                                <h3>Sinds 1956 uw inkoop gemak van bodem tot dak.<br />Uw partner voor non-ferro, leidingsystemen, dakbedekking, sanitair en appendages.</h3>
+                        <div class="col-md-7">
+                                <h3>Sinds 1956 uw inkoop gemak van bodem tot dak. Uw partner voor leidingsystemen, centrale verwarming, sanitair, non-ferro, dakbedekking en appendages.</h3>
                         </div>
                 </div>
         </header>
