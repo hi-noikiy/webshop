071e33fe3e6c5184bf7f5f42891a4f576d17d120
Luna Moonfang <thomas.wiringa@gmail.com>
2015-08-21 20:40:56 +0200

    Changed some stuff

--- resources/views/errors/csrf.blade.php
+++ resources/views/errors/csrf.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
         <div class="alert alert-danger" role="alert">
-                De sessie token is verlopen. Log opnieuw in en probeer het opnieuw.<br />
+                De sessie is verlopen. Log opnieuw in en probeer het opnieuw.<br />
                 <br />
                 Mogelijke oorzaak is:<br />
                 <br />
