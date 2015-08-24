1044fc851acfdddb98597ecc12c8ee540986202e
Luna Moonfang <thomas.wiringa@gmail.com>
2015-08-25 01:44:43 +0200

    Changed import success sentence

--- resources/views/admin/importsuccess.blade.php
+++ resources/views/admin/importsuccess.blade.php
@@ -14,7 +14,11 @@
                         <a href="/admin/import"><span class="glyphicon glyphicon-chevron-left"></span> Terug naar de import pagina</a>
 
                         <div class="text-center">
-                                <h4>De {{ Session::get('type') }} import is gelukt. Er {{ (Session::get('count') === 1 ? 'is' : 'zijn') }} {{ Session::get('count') }} regel{{ (Session::get('count') === 1 ? '' : 's') }} geimporteerd in {{ Session::get('time') }} seconden.</h4>
+                                @if (Session::get('type') === 'afbeelding')
+                                        <h4>De {{ Session::get('type') }} import is gelukt. Er {{ (Session::get('count') === 1 ? 'is' : 'zijn') }} {{ Session::get('count') }} {{ Session::get('type') }}{{ (Session::get('count') === 1 ? '' : 'en') }} geimporteerd in {{ Session::get('time') }} seconden.</h4>
+                                @else
+                                        <h4>De {{ Session::get('type') }} import is gelukt. Er {{ (Session::get('count') === 1 ? 'is' : 'zijn') }} {{ Session::get('count') }} regel{{ (Session::get('count') === 1 ? '' : 's') }} geimporteerd in {{ Session::get('time') }} seconden.</h4>
+                                @endif
                         </div>
                 </div>
         </div>
