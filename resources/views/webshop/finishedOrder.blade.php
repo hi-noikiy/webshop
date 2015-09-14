4bcbdd6462e0e25b9941a49a352c9ef82377e6a4
Luna Moonfang <thomas.wiringa@gmail.com>
2015-09-14 18:01:06 +0200

    Added finished order view

---
+++ resources/views/webshop/finishedOrder.blade.php
@@ -0,0 +1,17 @@
+@extends('master')
+
+@section('title')
+        <h3>Bedankt voor uw bestelling!</h3>
+@stop
+
+@section('content')
+	<div class="row">
+		<div class="col-md-12">
+			<p>
+				Wij hebben uw bestelling ontvangen en deze wordt zo spoedig mogelijk verwerkt.<Br />
+				<br />
+				<a href="/">Klik hier</a> om terug te gaan naar de hoofdpagina.
+			</p>
+		</div>
+	</div>
+@stop
