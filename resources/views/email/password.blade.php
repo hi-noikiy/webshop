62a2bc24086860004a6fc470a3fb70c6d8189853
Luna Moonfang <thomas.wiringa@gmail.com>
2015-09-19 00:30:08 +0200

    Added break lines

--- resources/views/email/password.blade.php
+++ resources/views/email/password.blade.php
@@ -1,5 +1,5 @@
-Iemand, hopelijk U, heeft een wachtwoord reset aangevraagd voor uw account.
-Klik op de volgende link om uw wachtwoord te resetten.
-Deze link is slechts 1 uur geldig.
-
+Iemand, hopelijk U, heeft een wachtwoord reset aangevraagd voor uw account.<br />
+Klik op de volgende link om uw wachtwoord te resetten.<br />
+Deze link is slechts 1 uur geldig.<br />
+<br />
 Klik hier om uw wachtwoord te resetten: {{ url('password/reset/'.$token) }}
