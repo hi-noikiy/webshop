fd9f78c5e4a193868a269ba7eab57699bac36c77
Luna Moonfang <thomas.wiringa@gmail.com>
2015-09-20 01:10:38 +0200

    Removed jQuery Toaster

--- resources/views/home/licenses.blade.php
+++ resources/views/home/licenses.blade.php
@@ -7,7 +7,7 @@
 @section('content')
         <h2><a href="http://laravel.com" target="_blank">Laravel</a> by <a href="https://github.com/taylorotwell" target="_blank">Taylor Otwell</a> <small>(<a href="https://github.com/laravel/framework/blob/4.2/LICENSE.txt" target="_blank">license</a>)</small></h2>
         <br />
-        <pre>
+        <div class="well">
         The MIT License (MIT)
 
         Copyright (c) Taylor Otwell
@@ -29,11 +29,11 @@
         LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
         OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
         THE SOFTWARE.
-        </pre>
+        </div>
         <hr />
         <h2><a href="https://github.com/barryvdh/laravel-debugbar" target="_blank">laravel-debugbar</a> by <a href="https://github.com/barryvdh" target="_blank">Barry vd. Heuvel</a> <small>(<a href="https://github.com/barryvdh/laravel-debugbar/blob/master/LICENSE" target="_blank">license</a>)</small></h2>
         <br />
-        <pre>
+        <div class="well">
         Copyright (C) 2013-2014 Barry vd. Heuvel
 
         Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -53,11 +53,11 @@
         LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
         OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
         SOFTWARE.
-        </pre>
+        </div>
         <hr />
         <h2><a href="https://github.com/Crinsane/LaravelShoppingcart" target="_blank">LaravelShoppingcart</a> by <a href="https://github.com/Crinsane" target="_blank">Rob Gloudemans</a> <small>(<a href="https://github.com/Crinsane/LaravelShoppingcart/blob/master/LICENSE" target="_blank">license</a>)</small></h2>
         <br />
-        <pre>
+        <div class="well">
         The MIT License (MIT)
 
         Copyright (c) 2014 Rob Gloudemans
@@ -79,11 +79,11 @@
         LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
         OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
         SOFTWARE.
-        </pre>
+        </div>
         <hr />
         <h2><a href="https://getbootstrap.com" target="_blank">Twitter Bootstrap</a> by <a href="https://github.com/mdo" target="_blank">Mark Otto</a> and <a href="https://github.com/fat" target="_blank">Jacob Thornton</a> <small>(<a href="https://github.com/twbs/bootstrap/blob/master/LICENSE" target="_blank">license</a>)</small></h2>
         <br />
-        <pre>
+        <div class="well">
         The MIT License (MIT)
 
         Copyright (c) 2011-2015 Twitter, Inc
@@ -105,11 +105,11 @@
         LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
         OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
         THE SOFTWARE.
-        </pre>
+        </div>
         <hr />
         <h2><a href="https://jquery.org" target="_blank">jQuery</a> by <a href="https://github.com/jquery" target="_blank">The jQuery Foundation</a> <small>(<a href="https://github.com/jquery/jquery/blob/master/LICENSE.txt" target="_blank">license</a>)</small></h2>
         <br />
-        <pre>
+        <div class="well">
         Copyright jQuery Foundation and other contributors, https://jquery.org/
         This software consists of voluntary contributions made by many
         individuals. For exact contribution history, see the revision history
@@ -142,26 +142,11 @@
         externally maintained libraries used by this software which have their
         own licenses; we recommend you read them, as their terms may differ from
         the terms above.
-        </pre>
-        <hr />
-        <h2><a href="https://github.com/scottoffen/jquery.toaster" target="_blank">jQuery Toaster</a> by <a href="https://github.com/scottoffen" target="_blank">Scott Offen</a> <small>(<a href="https://github.com/scottoffen/jquery.toaster/blob/master/LICENSE" target="_blank">license</a>)</small></h2>
-        <br />
-        <pre>
-        Copyright 2014 Scott Offen
-
-        Licensed under the Apache License, Version 2.0 (the "License");
-        you may not use this file except in compliance with the License.
-        You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-
-        Unless required by applicable law or agreed to in writing,
-        software distributed under the License is distributed on an "AS IS" BASIS,
-        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-        See the License for the specific language governing permissions and limitations under the License.
-        </pre>
+        </div>
         <hr />
         <h2><a href="http://glyphicons.com" target="_blank">Glyphicons</a> by <a href="https://twitter.com/GLYPHICONS" target="_blank">Jan Kovařík</a> <small>(<a href="https://github.com/scottoffen/jquery.toaster/blob/master/LICENSE" target="_blank">license</a>)</small></h2>
         <br />
-        <pre>
+        <div class="well">
         The MIT License (MIT)
 
         Copyright (c) 2011-2015 Jan Kovařík
@@ -183,12 +168,12 @@
         LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
         OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
         THE SOFTWARE.
-        </pre>
+        </div>
         <hr />
         <h2><a href="http://ckeditor.com" target="_blank">CKEditor</a> by <a href="https://github.com/orgs/ckeditor/people" target="_blank">the CKEditor Team</a> <small>(<a href="https://github.com/ckeditor/ckeditor-releases/blob/master/LICENSE.md" target="_blank">license</a>)</small></h2>
         <br />
-        <pre>
-        CKEditor - The text editor for Internet - http://ckeditor.com Copyright (c) 2003-2015, CKSource - Frederico Knabben. 
+        <div class="well">
+        CKEditor - The text editor for Internet - http://ckeditor.com Copyright (c) 2003-2015, CKSource - Frederico Knabben.
         All rights reserved.
 
         Licensed under the terms of any of the following licenses at your choice:
@@ -199,11 +184,23 @@
 
         Mozilla Public License Version 1.1 or later (the "MPL") http://www.mozilla.org/MPL/MPL-1.1.html (See Appendix C)
 
-        You are not required to, but if you want to explicitly declare the license you have 
-        chosen to be bound to when using, reproducing, modifying and distributing this software, 
-        just include a text file titled "legal.txt" in your version of this software, indicating 
-        your license choice. In any case, your choice will not restrict any recipient of your 
-        version of this software to use, reproduce, modify and distribute this software under 
+        You are not required to, but if you want to explicitly declare the license you have
+        chosen to be bound to when using, reproducing, modifying and distributing this software,
+        just include a text file titled "legal.txt" in your version of this software, indicating
+        your license choice. In any case, your choice will not restrict any recipient of your
+        version of this software to use, reproduce, modify and distribute this software under
         any of the above licenses.
-        </pre>
+        </div>
+@stop
+
+@section('extraCSS')
+        <style>
+                .well {
+                        white-space: pre;
+                }
+
+                h2 {
+                        text-align: center;
+                }
+        </style>
 @stop
