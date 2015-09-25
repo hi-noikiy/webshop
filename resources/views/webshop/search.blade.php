189044cb779d77b825133af73e97bab1759ea8a6
Luna Moonfang <thomas.wiringa@gmail.com>
2015-09-25 15:48:33 +0200

    Added search test

--- resources/views/webshop/search.blade.php
+++ resources/views/webshop/search.blade.php
@@ -5,119 +5,125 @@
 @stop
 
 @section('content')
-        <div class="alert alert-success" role="alert">
-                {{ $results->total() }} resultaten gevonden in {{ $scriptTime }} seconden.
-        </div>
-        <div class="panel panel-default">
-                <div class="panel-heading">
-                        <h4 class="panel-title text-center">
-                                Geavanceerd zoeken
-                        </h4>
+        @if ($results->total() === 0)
+                <div class="alert alert-warning" role="alert">
+                        Er zijn geen resultaten gevonden voor deze zoekopdracht
                 </div>
-                <div class="panel-body">
-                        <form action="/search" method="GET" role="search" class="form-horizontal" name="advancedsearch" id="searchForm">
+        @else
+                <div class="alert alert-success" role="alert">
+                        {{ $results->total() }} resultaten gevonden in {{ $scriptTime }} seconden.
+                </div>
+                <div class="panel panel-default">
+                        <div class="panel-heading">
+                                <h4 class="panel-title text-center">
+                                        Geavanceerd zoeken
+                                </h4>
+                        </div>
+                        <div class="panel-body">
+                                <form action="/search" method="GET" role="search" class="form-horizontal" name="advancedsearch" id="searchForm">
+                                        <div class="row">
+                                                <div class="col-md-4">
+                                                        <div class="form-group">
+                                                                <label class="col-sm-2 control-label">Merk</label>
+                                                                <div class="col-sm-10">
+                                                                        <select onchange="wtg.quickSearch();" name="brand" class="form-control">
+                                                                                <option value="">----------</option>
+                                                                                @foreach($brands as $brand)
+                                                                                        <option @if(Input::get('brand') === $brand) selected @endif value="{{ $brand }}">{{ $brand }}</option>
+                                                                                @endforeach
+                                                                        </select>
+                                                                </div>
+                                                        </div>
+                                                </div>
+                                                <div class="col-md-4">
+                                                        <div class="form-group">
+                                                                <label class="col-sm-2 control-label">Serie</label>
+                                                                <div class="col-sm-10">
+                                                                        <select onchange="wtg.quickSearch();" name="serie" class="form-control">
+                                                                                <option value="">----------</option>
+                                                                                @foreach($series as $serie)
+                                                                                        <option @if(Input::get('serie') === $serie) selected @endif value="{{ $serie }}">{{ $serie }}</option>
+                                                                                @endforeach
+                                                                        </select>
+                                                                </div>
+                                                        </div>
+                                                </div>
+                                                <div class="col-md-4">
+                                                        <div class="form-group">
+                                                                <label class="col-sm-2 control-label">Type</label>
+                                                                <div class="col-sm-10">
+                                                                        <select onchange="wtg.quickSearch();" name="type" class="form-control">
+                                                                                <option value="">----------</option>
+                                                                                @foreach($types as $type)
+                                                                                        <option @if(Input::get('type') === $type) selected @endif value="{{ $type }}">{{ $type }}</option>
+                                                                                @endforeach
+                                                                        </select>
+                                                                </div>
+                                                        </div>
+                                                </div>
+                                        </div>
+                                        <input name="q" type="hidden" value="{{ Input::get('q') }}">
+                                </form>
+
                                 <div class="row">
-                                        <div class="col-md-4">
-                                                <div class="form-group">
-                                                        <label class="col-sm-2 control-label">Merk</label>
-                                                        <div class="col-sm-10">
-                                                                <select onchange="wtg.quickSearch();" name="brand" class="form-control">
-                                                                        <option value="">----------</option>
-                                                                        @foreach($brands as $brand)
-                                                                                <option @if(Input::get('brand') === $brand) selected @endif value="{{ $brand }}">{{ $brand }}</option>
-                                                                        @endforeach
-                                                                </select>
-                                                        </div>
-                                                </div>
+                                        <div class="col-sm-6">
+                                                <button onclick="window.history.go(-1);" class="btn btn-default btn-block"><span class="glyphicon glyphicon-chevron-left"></span> Terug naar vorige pagina</button>
                                         </div>
-                                        <div class="col-md-4">
-                                                <div class="form-group">
-                                                        <label class="col-sm-2 control-label">Serie</label>
-                                                        <div class="col-sm-10">
-                                                                <select onchange="wtg.quickSearch();" name="serie" class="form-control">
-                                                                        <option value="">----------</option>
-                                                                        @foreach($series as $serie)
-                                                                                <option @if(Input::get('serie') === $serie) selected @endif value="{{ $serie }}">{{ $serie }}</option>
-                                                                        @endforeach
-                                                                </select>
-                                                        </div>
-                                                </div>
+
+                                        <br class="visible-xs" />
+
+                                        <div class="col-sm-6">
+                                                <button class="btn btn-primary pull-right btn-block" onclick="wtg.quickSearch();">Zoeken</button>
                                         </div>
-                                        <div class="col-md-4">
-                                                <div class="form-group">
-                                                        <label class="col-sm-2 control-label">Type</label>
-                                                        <div class="col-sm-10">
-                                                                <select onchange="wtg.quickSearch();" name="type" class="form-control">
-                                                                        <option value="">----------</option>
-                                                                        @foreach($types as $type)
-                                                                                <option @if(Input::get('type') === $type) selected @endif value="{{ $type }}">{{ $type }}</option>
-                                                                        @endforeach
-                                                                </select>
-                                                        </div>
-                                                </div>
-                                        </div>
-                                </div>
-                                <input name="q" type="hidden" value="{{ Input::get('q') }}">
-                        </form>
-
-                        <div class="row">
-                                <div class="col-sm-6">
-                                        <button onclick="window.history.go(-1);" class="btn btn-default btn-block"><span class="glyphicon glyphicon-chevron-left"></span> Terug naar vorige pagina</button>
-                                </div>
-
-                                <br class="visible-xs" />
-
-                                <div class="col-sm-6">
-                                        <button class="btn btn-primary pull-right btn-block" onclick="wtg.quickSearch();">Zoeken</button>
                                 </div>
                         </div>
                 </div>
-        </div>
 
 
-        <table class="table table-striped">
-                <thead>
-                        <tr>
-                                <th></th>
-                                <th class="hidden-xs">Artikelnummer</th>
-                                <th>Omschrijving</th>
-                                @if(Auth::check())
-                                        <th class="hidden-xs">Bruto prijs</th>
-                                        <th class="hidden-xs">Korting</th>
-                                        <th>Netto prijs</th>
-                                @endif
-                        </tr>
-                </thead>
-                <tbody>
-                        <?php $discounts = (Auth::check() ? getProductDiscount(Auth::user()->login) : ''); ?>
-                        @foreach($results as $product)
-                                <?php $price = number_format((preg_replace("/\,/", ".", $product->price) * $product->refactor) / $product->price_per, 2, ".", ""); ?>
-
-                                @if(isset($discounts[$product->number]))
-                                        <?php $discount = $discounts[$product->number]; ?>
-                                @elseif(isset($discounts[$product->group]))
-                                        <?php $discount = $discounts[$product->group]; ?>
-                                @else
-                                        <?php $discount = 0; ?>
-                                @endif
-
-                                <tr {{ ($discount === '0' ? 'class=success' : '') }}>
-                                        <td class="product-thumbnail"><img src="/img/products/{{ $product->image }}" alt="{{ $product->image }}"></td>
-                                        <td class="hidden-xs">{{ $product->number }}</td>
-                                        <td><a href="/product/{{ $product->number }}">{{ $product->name }}</a></td>
+                <table class="table table-striped">
+                        <thead>
+                                <tr>
+                                        <th></th>
+                                        <th class="hidden-xs">Artikelnummer</th>
+                                        <th>Omschrijving</th>
                                         @if(Auth::check())
-                                                <td class="hidden-xs">&euro;{{ $price }}</td>
-                                                <td class="hidden-xs">{{ ($discount === '0' ? 'Actie' : $discount . '%') }}</td>
-                                                <td>&euro;{{ number_format($price * ((100-$discount) / 100), 2, ".", "") }}</td>
+                                                <th class="hidden-xs">Bruto prijs</th>
+                                                <th class="hidden-xs">Korting</th>
+                                                <th>Netto prijs</th>
                                         @endif
                                 </tr>
-                        @endforeach
-                </tbody>
-        </table>
+                        </thead>
+                        <tbody>
+                                <?php $discounts = (Auth::check() ? getProductDiscount(Auth::user()->login) : ''); ?>
+                                @foreach($results as $product)
+                                        <?php $price = number_format((preg_replace("/\,/", ".", $product->price) * $product->refactor) / $product->price_per, 2, ".", ""); ?>
 
-        <div class="text-center">
-                {!! $results->appends(array('brand' => Input::get('brand'), 'serie' => Input::get('serie'), 'type' => Input::get('type'), 'q' => Input::get('q')))->render() !!}
-        </div>
+                                        @if(isset($discounts[$product->number]))
+                                                <?php $discount = $discounts[$product->number]; ?>
+                                        @elseif(isset($discounts[$product->group]))
+                                                <?php $discount = $discounts[$product->group]; ?>
+                                        @else
+                                                <?php $discount = 0; ?>
+                                        @endif
+
+                                        <tr {{ ($discount === '0' ? 'class=success' : '') }}>
+                                                <td class="product-thumbnail"><img src="/img/products/{{ $product->image }}" alt="{{ $product->image }}"></td>
+                                                <td class="hidden-xs">{{ $product->number }}</td>
+                                                <td><a href="/product/{{ $product->number }}">{{ $product->name }}</a></td>
+                                                @if(Auth::check())
+                                                        <td class="hidden-xs">&euro;{{ $price }}</td>
+                                                        <td class="hidden-xs">{{ ($discount === '0' ? 'Actie' : $discount . '%') }}</td>
+                                                        <td>&euro;{{ number_format($price * ((100-$discount) / 100), 2, ".", "") }}</td>
+                                                @endif
+                                        </tr>
+                                @endforeach
+                        </tbody>
+                </table>
+
+                <div class="text-center">
+                        {!! $results->appends(array('brand' => Input::get('brand'), 'serie' => Input::get('serie'), 'type' => Input::get('type'), 'q' => Input::get('q')))->render() !!}
+                </div>
+        @endif
 @stop
 
 @section('extraJS')
---
+++ tests/searchTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class searchTest extends TestCase
+{
+        /**
+        * A basic test example.
+        *
+        * @return void
+        */
+        public function testSearchForExistingProduct()
+        {
+                $this->visit('/webshop')
+                     ->type('buis', 'q')
+                     ->press('Zoeken')
+                     ->see('resultaten gevonden in');
+        }
+
+        /**
+        * A basic test example.
+        *
+        * @return void
+        */
+        public function testSearchForNonExistingProduct()
+        {
+                $this->visit('/webshop')
+                     ->type('341ktm13l4', 'q')
+                     ->press('Zoeken')
+                     ->see('Geen resultaten gevonden');
+        }
+}
