90f30bf990f50f6e8c741ae749f71d3d41eb1013
Luna Moonfang <thomas.wiringa@gmail.com>
2015-10-02 22:03:22 +0200

    Changed maintenance mode view

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -3,7 +3,7 @@
     <head>
         <title>Be right back.</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Titillium+Web:100" rel="stylesheet" type="text/css">
 
         <style>
             html, body {
@@ -17,7 +17,7 @@
                 color: #B0BEC5;
                 display: table;
                 font-weight: 100;
-                font-family: 'Lato';
+                font-family: 'Titillium Web';
             }
 
             .container {
@@ -40,7 +40,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">Tijdelijk onderhoud.</div>
             </div>
         </div>
     </body>
