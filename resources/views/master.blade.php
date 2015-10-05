eac3bab56376a2b3980ca144f48d864b3a580391
Thomas Wiringa <DuckThom@users.noreply.github.com>
2015-10-05 18:27:00 +0200

    Moved google analytics code to the head

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -16,6 +16,17 @@
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
+        
+        <script>
+                // Google Analytics
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                
+                ga('create', 'UA-41373177-1', 'auto');
+                ga('send', 'pageview');
+        </script>
 </head>
 <body>
         <div class="background"></div>
@@ -213,15 +224,6 @@
                                 setTimeout(function() {
                                         $('#statusmessage').slideUp();
                                 }, 5000);
-                                
-                                // Google Analytics
-                                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-                                
-                                ga('create', 'UA-41373177-1', 'auto');
-                                ga('send', 'pageview');
                         </script>
                 </footer>
         </div>
