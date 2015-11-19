ddde231c58f2664e09ed88467fe20e2065d2128c
Luna Moonfang <thomas.wiringa@gmail.com>
2015-11-19 22:56:04 +0100

    :balloon: Add missing favicon

--- resources/views/errors/master.blade.php
+++ resources/views/errors/master.blade.php
@@ -12,6 +12,9 @@
         <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,600,700' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
 
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -14,6 +14,9 @@
 
         <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,600,700' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+        
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
