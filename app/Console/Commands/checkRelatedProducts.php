ff5ff716fa497fb5d21279d8521202dbb9e26480
Thomas Wiringa <thomas.wiringa@gmail.com>
2015-12-14 14:18:51 +0100

    Refine progress bar

--- app/Console/Commands/checkRelatedProducts.php
+++ app/Console/Commands/checkRelatedProducts.php
@@ -41,14 +41,18 @@ class checkRelatedProducts extends Command
         $errors = "";
         $products = Product::select(['number', 'related_products'])->where('related_products', '!=', '')->get();
         $bar = $this->output->createProgressBar(count($products));
+        $bar->setFormat(' %current%/%max% [%bar%] %percent:3s%% %memory:6s% %product%');
 
         // Loop through every product that has related products
         foreach ($products as $product) {
+
+            $bar->setMessage($product->number, 'product');
+
             // Loop though the related products to check if they exist
             foreach (explode(",", $product->related_products) as $related_product) {
                 // Throw an error in the user's face if the related product does not exist
                 if (Product::where('number', $related_product)->count() === 0)
-                    $errors .= "Product " . $product->number . " heeft een niet-gerelateerd product: " . $related_product . "\r\n";
+                    $errors .= "Product " . $product->number . " has a non-existing related product: " . $related_product . "\r\n";
             }
 
             $bar->advance();
@@ -59,8 +63,10 @@ class checkRelatedProducts extends Command
         $this->line("\r\n");
 
         if ($errors !== "")
-            $this->error($errors);
+            $this->error("\r\n \r\n" . $errors);
         else
-            $this->info("Er zijn geen fouten gevonden");
+            $this->info("\r\nEr zijn geen fouten gevonden");
+
+        $this->line("\r\n");
     }
 }
