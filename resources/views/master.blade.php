6cc8f13d1f776d3611ae4bf821844255d8b985bb
Thomas Wiringa <thomas.wiringa@gmail.com>
2015-12-25 20:31:39 +0100

    Change less to sass

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -12,7 +12,7 @@
 
     @yield('extraCSS')
 
-    <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,600,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
 
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
@@ -209,8 +209,8 @@
                 </p>
             </div>
 
-            <script src="{{ elixir('js/jquery.min.js') }}"></script>
-            <script src="{{ elixir('js/bootstrap.min.js') }}"></script>
+            <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
             @yield('extraJS')
 
