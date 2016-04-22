d81462fc6b02abf38e9434535dbc7a382757a20f
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-04-22 12:25:38 +0200

    Add basic dynamic content test

---
+++ tests/DynamicContentTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class DynamicContentTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testNewsOnHomepage()
+    {
+        $message = 'This is a test message';
+
+        $content = \App\Content::where('name', 'home.news')->first();
+        $content->content = $message;
+        $content->save();
+
+        $this->visit('/')->see($message);
+    }
+}
