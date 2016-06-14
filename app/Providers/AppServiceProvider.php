597467a169a66eeeb71621f6406845750591d951
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-06-14 13:55:39 +0200

    Don't send log messages as exceptions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,36 +12,7 @@ class AppServiceProvider extends ServiceProvider {
 	 */
 	public function boot()
 	{
-        \Event::listen('illuminate.log', function($level, $message) {
-            switch ($level) {
-                case 'emergency':
-                case 'alert':
-                    $level = 'fatal';
-                    break;
-                case 'critical':
-                    $level = 'error';
-                    break;
-                case 'notice':
-                    $level = 'info';
-                    break;
-            }
-
-            // Create a sentry variable
-            $sentry = app('sentry');
-
-            // Add the user login if someone is logged in
-            if (auth()->check()) {
-                $sentry->user_context([
-                    'id'        => auth()->user()->login,
-                    'username'  => auth()->user()->company
-                ]);
-            }
-            
-            $sentry->captureException(new LogException($message), [
-                'level' => $level
-            ]);
-
-        });
+        //
 	}
 
 	/**
