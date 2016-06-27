72e413d9b945e76a560dc5a23052d07ddd2ba8ae
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-06-27 12:55:15 +0200

    Add experimental method to get a product discount

--- app/Product.php
+++ app/Product.php
@@ -31,4 +31,19 @@ class Product extends Model {
     {
         return Pack::where('product_number', $this->number)->count() === 1;
     }
+
+	/**
+	 * Get the discount for a product
+	 *
+	 * @return int
+	 */
+	public function discount()
+	{
+		$discount = Discount::select([DB::raw('MAX(discount) as value')])->where('User_id', \Auth::user()->login)->where(function ($query) {
+			$query->whereProduct($this->number)
+				->orWhere('product', $this->group);
+		})->first();
+
+		return (int) $discount->value;
+	}
 }
