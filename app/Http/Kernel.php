13dd70d4d30bcf830b128028953d1fa4fc7f98d6
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-08-22 15:46:46 +0200

    Add check manager middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -11,11 +11,11 @@ class Kernel extends HttpKernel {
 	 */
 	protected $middleware = [
 		\Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-	        \App\Http\Middleware\EncryptCookies::class,
-	        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-	        \Illuminate\Session\Middleware\StartSession::class,
-	        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-	        \App\Http\Middleware\VerifyCsrfToken::class,
+        \App\Http\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \App\Http\Middleware\VerifyCsrfToken::class,
 	];
 
 	/**
@@ -29,7 +29,8 @@ class Kernel extends HttpKernel {
 		'auth.admin' => \App\Http\Middleware\AuthenticateAdmin::class,
 		'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
 		'RemoveTempFile' => \App\Http\Middleware\RemoveTempFile::class,
-        'ajax' => \App\Http\Middleware\CheckAjaxRequest::class
+        'ajax' => \App\Http\Middleware\CheckAjaxRequest::class,
+        'manager' => \App\Http\Middleware\CheckManager::class
 	];
 
 }
