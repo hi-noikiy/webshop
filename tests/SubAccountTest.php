53af733cd8a5511a1bbb2000598f98ea24638521
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-09-05 20:33:58 +0200

    Add test for sub account creation

--- tests/SubAccountTest.php
+++ tests/SubAccountTest.php
@@ -11,7 +11,6 @@ class SubAccountTest extends TestCase
 {
     use DatabaseTransactions;
 
-
     /**
      * Test the product method with an existing product
      *
@@ -20,9 +19,9 @@ class SubAccountTest extends TestCase
     public function testIfManagerHasSubAccountsNavigationItem()
     {
         $this->createCompany();
-        $this->createUser(false, true);
+        $user = $this->createUser(false, true);
 
-        $this->actingAs(User::whereUsername('12345')->first())
+        $this->actingAs($user)
             ->visit('account')
             ->see('Sub-Accounts');
     }
@@ -35,10 +34,54 @@ class SubAccountTest extends TestCase
     public function testIfNormalAccountCannotAccessManagerRoute()
     {
         $this->createCompany();
-        $this->createUser(false, false);
+        $user = $this->createUser(false, false);
 
-        $this->actingAs(User::whereUsername('12345')->first())
+        $this->actingAs($user)
             ->visit('account')
             ->dontSee('Sub-Accounts');
+    }
+
+    public function testSubAccountCreation()
+    {
+        $this->createCompany();
+        $user = $this->createUser(false, true);
+
+        $this->actingAs($user)
+            ->visit('account/accounts')
+            ->type('username', 'username')
+            ->type('email@example.com', 'email')
+            ->type('password', 'password')
+            ->type('password', 'password_confirmation')
+            ->press('Toevoegen')
+            ->see('Het sub account is aangemaakt');
+
+        $this->assertTrue(\Auth::validate(['username' => 'username', 'company_id' => 12345, 'password' => 'password']));
+    }
+
+    public function testDuplicateSubAccountCreation()
+    {
+        $this->createCompany();
+        $user = $this->createUser(false, true);
+
+        $this->actingAs($user)
+            ->visit('account/accounts')
+            ->type('username', 'username')
+            ->type('email@example.com', 'email')
+            ->type('password', 'password')
+            ->type('password', 'password_confirmation')
+            ->press('Toevoegen')
+            ->see('Het sub account is aangemaakt');
+
+        $this->actingAs($user)
+            ->visit('account/accounts')
+            ->type('username', 'username')
+            ->type('email2@example.com', 'email')
+            ->type('another_password', 'password')
+            ->type('another_password', 'password_confirmation')
+            ->press('Toevoegen')
+            ->see('Er bestaat al een sub account met deze login naam');
+
+        $this->assertTrue(\Auth::validate(['username' => 'username', 'company_id' => 12345, 'password' => 'password']));
+        $this->assertFalse(\Auth::validate(['username' => 'username', 'company_id' => 12345, 'password' => 'another_password']));
     }
 }
