1025281780ecfa0002c23708dce7621fdbddf05f
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-09-05 20:03:37 +0200

    Change route

--- resources/views/account/sidebar.blade.php
+++ resources/views/account/sidebar.blade.php
@@ -3,7 +3,7 @@
     @if (Auth::user()->manager)
         <a href="{{ url('account/accounts') }}" class="list-group-item @if( Route::current()->getUri() === 'account/accounts' ) active @endif">Sub-accounts</a>
     @endif
-    <a href="{{ url('account/changepassword') }}" class="list-group-item @if( Route::current()->getUri() === 'account/changepassword' ) active @endif">Wachtwoord wijzigen</a>
+    <a href="{{ route('change_password') }}" class="list-group-item {{ (Route::is('change_password') ? 'active' : '') }}">Wachtwoord wijzigen</a>
     <a href="{{ url('account/favorites') }}" class="list-group-item @if( Route::current()->getUri() === 'account/favorites' ) active @endif">Favorieten</a>
     <a href="{{ url('account/orderhistory') }}" class="list-group-item @if( Route::current()->getUri() === 'account/orderhistory' ) active @endif">Bestelgeschiedenis</a>
     <a href="{{ url('account/addresslist') }}" class="list-group-item @if( Route::current()->getUri() === 'account/addresslist' ) active @endif">Adressenlijst</a>
