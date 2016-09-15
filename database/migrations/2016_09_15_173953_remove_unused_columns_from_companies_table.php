1f2e35029f82d2989aae0b1bf4fdf0324e3d0adf
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-09-15 19:50:08 +0200

    Add migration to remove unused columns from companies table

---
+++ database/migrations/2016_09_15_173953_remove_unused_columns_from_companies_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveUnusedColumnsFromCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->dropColumn('street');
+            $table->dropColumn('postcode');
+            $table->dropColumn('city');
+            $table->dropColumn('email');
+            $table->dropColumn('password');
+            $table->dropColumn('favorites');
+            $table->dropColumn('cart');
+            $table->dropColumn('remember_token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->string('street', 100);
+            $table->string('postcode', 7);
+            $table->string('city', 50);
+            $table->string('email', 50);
+            $table->string('password');
+            $table->string('favorites', 8000)->default('a:0:{}');
+            $table->mediumText('cart')->nullable();
+            $table->rememberToken();
+        });
+    }
+}
