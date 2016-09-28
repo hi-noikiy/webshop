d1ac9c965f643469ae8053cacf1e59d352101bd3
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-09-28 18:26:34 +0200

    Fix more code styling

--- app/Exceptions/ProductNotFoundException.php
+++ app/Exceptions/ProductNotFoundException.php
@@ -9,10 +9,20 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
  */
 class ProductNotFoundException extends HttpException
 {
+
+    /**
+     * ProductNotFoundException constructor.
+     *
+     * @param  string  $id
+     * @param  \Exception|null  $previous
+     * @param  array  $headers
+     * @param  int  $code
+     */
     public function __construct($id, \Exception $previous = null, array $headers = [], $code = 0)
     {
         $message = "Geen product gevonden met nummer '{$id}'";
 
         parent::__construct(404, $message, $previous, $headers, $code);
     }
+
 }
